966258c8733a7c4d0c1d3137973d36851df84ed7
Aruna Lakmal <arunarandunu@gmail.com>
2019-11-29 23:21:11 +0530

    route table association fix

--- main.tf
+++ main.tf
@@ -112,11 +112,11 @@ resource "aws_route_table_association" "hbr_public1_rt_assoc" {
 }
 resource "aws_route_table_association" "hbr_public2_rt_assoc" {
   subnet_id      = "${aws_subnet.hbr_public2_subnet.id}"
-  route_table_id = "${aws_subnet.hbr_public2_subnet.id}"
+  route_table_id = "${aws_route_table.hbr_pub_rt.id}"
 }
 resource "aws_route_table_association" "hbr_private1_rt_assoc" {
   subnet_id      = "${aws_subnet.hbr_private1_subnet.id}"
-  route_table_id = "${aws_subnet.hbr_private1_subnet.id}"
+  route_table_id = "${aws_route_table.hbr_pvt_rt.id"
 }
 
 #---- Security Group ----
