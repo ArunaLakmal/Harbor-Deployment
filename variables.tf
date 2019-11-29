ec554ec86b6961959dd26297c9a24c8ffe3c3ed4
Aruna Lakmal <arunarandunu@gmail.com>
2019-11-29 22:48:45 +0530

    main.tf

--- variables.tf
+++ variables.tf
@@ -3,9 +3,6 @@ variable "vpc_cidr" {}
 variable "cidrs" {
   type = "map"
 }
-data "aws_availability_zones" "available" {
-  state = "available"
-}
 variable "hbr_key_name" {}
 variable "public_key_path" {}
 variable "hbr_instance" {}
